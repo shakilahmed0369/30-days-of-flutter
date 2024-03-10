f4a1cba72cd85b610a8dab34a544cc4cf57a8dbc
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-03-10 22:27:59 +0600

    day-8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,11 +42,37 @@ class _HomePageState extends State<HomePage> {
         title: Text("Catelog App!"),
       ),
       body: (CatalogModel.items.isNotEmpty)
-          ? ListView.builder(
-              itemCount: CatalogModel.items.length,
+          ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16),
               itemBuilder: (context, index) {
-                return ItemWidget(item: CatalogModel.items[index]);
+                final item = CatalogModel.items[index];
+                return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(item.name),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ));
               },
+              itemCount: CatalogModel.items.length,
             )
           : Center(child: CircularProgressIndicator()),
       drawer: MyDrawer(),
